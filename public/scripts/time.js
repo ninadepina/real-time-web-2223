b5a07ffeb3d99479cf766700c363629b97c1d1f6
Nina Vens <nina.vens@hva.nl>
2023-04-18 16:37:19 +0200

    [added] get time

---
+++ public/scripts/time.js
@@ -0,0 +1,18 @@
+const format = (n) => {
+	return n < 10 ? `0${n}` : n;
+};
+
+const timeData = () => {
+	const date = new Date();
+	const data = {
+		year: date.getFullYear().toString(),
+		month: format(date.getMonth() + 1).toString(),
+		day: format(date.getDate()).toString(),
+		h: format(date.getHours()).toString(),
+		m: format(date.getMinutes()).toString(),
+		s: format(date.getSeconds()).toString()
+	};
+	return data;
+};
+
+export default timeData;
