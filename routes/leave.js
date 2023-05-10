90d2bf9fed3b194d27a858d314c634c4ef4b2aed
Nina Vens <nina.vens@hva.nl>
2023-05-10 16:04:36 +0200

    [added] leave route

---
+++ routes/leave.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	req.session.destroy();
+	res.redirect('/');
+});
+
+export default router;
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 import home from '../routes/home.js';
 import room from '../routes/room.js';
 import username from '../routes/username.js';
+import leave from '../routes/leave.js';
 import error from '../routes/error.js';
 
 const routes = [
 	{ path: '/', view: home },
 	{ path: '/room', view: room },
 	{ path: '/username', view: username }
+	{ path: '/leave', view: leave },
 	{ path: '*', view: error }
 ];
 
