154ee4e08ccd6a02fed15dddd90d13a013d94edf
Nina Vens <nina.vens@hva.nl>
2023-05-10 19:46:07 +0200

    [changed] copy on click

--- public/scripts/copyOnClick.js
+++ public/scripts/copyOnClick.js
@@ -1,12 +1,13 @@
 const roomId = document.querySelector('.roomId h2 span');
 
-roomId.addEventListener('click', (e) => {
-	navigator.clipboard
-		.writeText(roomId.textContent)
-		.then(() => {
-			console.log('ROOM ID copied to clipboard');
-		})
-		.catch((err) => {
-			console.error('Error copying ROOM ID: ', err);
-		});
+roomId.addEventListener('click', async () => {
+	try {
+		await navigator.clipboard.writeText(roomId.textContent);
+		console.log('ROOM ID copied to clipboard');
+	} catch (err) {
+		console.error('Error copying ROOM ID:', err);
+	} finally {
+		roomId.setAttribute('data-tooltip', 'copied to clipboard');
+		setTimeout(() => roomId.setAttribute('data-tooltip', ''), 500);
+	}
 });
