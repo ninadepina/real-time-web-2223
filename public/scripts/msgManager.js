2acff46b109827aab5ffb682880f0516748d94bb
Nina Vens <nina.vens@hva.nl>
2023-05-12 16:20:03 +0200

    [cleaned]

--- public/scripts/msgManager.js
+++ public/scripts/msgManager.js
@@ -10,22 +10,20 @@ function htmlStr(obj) {
 		case 'chat_message':
 			const allMessages = messages.querySelectorAll('li');
 			const lastMsg = allMessages[allMessages.length - 1];
-			if (allMessages.length === 0 || lastMsg.id !== obj.sender.username) {
-				result = `<div><h3>${obj.sender.username}</h3><h4>${data.h}:${data.m}</h4></div><p>${obj.message}</p>`;
-			} else {
-				result = `<p>${obj.message}</p>`;
-			}
+			result =
+				allMessages.length === 0 || lastMsg.id !== obj.sender.username
+					? `<div><h3>${obj.sender.username}</h3><h4>${data.h}:${data.m}</h4></div><p>${obj.message}</p>`
+					: `<p>${obj.message}</p>`;
 			break;
 		case 'system_message_game':
 			const msg = obj.message.split(' VS ');
 			result = `<div><span><p>${msg[0]}</p><img src="/public/uploads/x2.svg" draggable="false"/></span><p> VS </p><span><img src="/public/uploads/o2.svg" draggable="false"/><p>${msg[1]}</p></span></div>`;
 			break;
 		case 'system_message_result':
-			if (obj.message === 'DRAW') {
-				result = `<div><h2>draw</h2></div>`;
-			} else {
-				result = `<div><h2>winner: </h2><p>${obj.message}</p></div>`;
-			}
+			result =
+				obj.message === 'DRAW'
+					? `<div><h2>draw</h2></div>`
+					: `<div><h2>winner: </h2><p>${obj.message}</p></div>`;
 			break;
 		case 'system_message_pussy':
 			result = `<div><img src="/public/uploads/${obj.player}2.svg" draggable="false"/><p>${obj.message}</p></div>`;
@@ -60,9 +58,7 @@ export function onMessage(obj, currentUser) {
 			const allMessages = messages.querySelectorAll('li');
 			const lastMsg = allMessages[allMessages.length - 1];
 
-			if (allMessages.length > 0 && lastMsg.id === obj.sender.username) {
-				msg.classList.add('double');
-			}
+			if (allMessages.length > 0 && lastMsg.id === obj.sender.username) msg.classList.add('double');
 			msg.classList.add(obj.sender.username === currentUser ? 'currentUser' : 'otherUser');
 			break;
 	}
